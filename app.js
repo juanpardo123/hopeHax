f75df2bda10ac4a2e2106283fd219c14aff38587
juanpardo123 <mrstudent.bear@gmail.com>
2023-06-15 18:53:17 -0400

    working search function

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import fs from 'fs';
 import http from 'https'
 import express from 'express';
+import axios from 'axios';
 import bodyParser from 'body-parser';
-//import {getfoods, getfoodsByID, createUser} from './database'
+import {getfoods, getfoodsByID, createUser} from './database.js'
 
 const app = express();
 const port = 3000;
+
+const app_id = 'e74cfc4c';
+const app_key = 'f2390ae95179e70da980b011ef24d3f1'
 
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -25,17 +29,28 @@ app.get('/', (req,res)=>{
     res.render('index');
 })
 
+app.post('/search', async (req, res) => {
+    try {
+      let search = req.body.name;
+      console.log(search);
+      let result = await getItemApi(search);
+      console.log(result);
+      res.render('singleItem', {data:result});
+    } catch (error) {
+      console.error(error);
+      res.render('error');
+    }
+  });
+  
+  async function getItemApi(ingredient) {
+    const url = `https://api.edamam.com/api/food-database/v2/parser?app_id=${app_id}&app_key=${app_key}&ingr=${ingredient}`;
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+      return data.parsed[0].food;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
 //Api implementation
-const app_id = 'e74cfc4c';
-const app_key = 'f2390ae95179e70da980b011ef24d3f1'
-const ingredient = "apple"
-const url = `https://api.edamam.com/api/food-database/v2/parser?app_id=${app_id}&app_key=${app_key}&ingr=${ingredient}`;
-
-
-import axios from 'axios';
-
-axios.get(url)
-  .then(response => {
-    const data = response.data;
-console.log (data.parsed[0].food)
-console.log (data.parsed[0].food.nutrients)})
