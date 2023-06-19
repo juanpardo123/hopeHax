067007b8a29619450007a3cca538d462352082b3
juanpardo123 <mrstudent.bear@gmail.com>
2023-06-19 10:59:09 -0400

    commentation

--- database.js
+++ database.js
@@ -1,3 +1,11 @@
+//Important terms:
+//Hashing:
+// Hashing is a one-way process of converting an input (such as a password or any other data) into a fixed-length string of characters. The resulting string, known as the hash value or digest, is unique to the input data. Hash functions are designed to be computationally efficient, producing a hash value quickly.
+
+// Salting:
+// Salting involves adding a random string of characters, known as a salt, to the input data before hashing. The salt is unique for each piece of data being hashed. The purpose of salting is to prevent attackers from using precomputed tables, such as rainbow tables, to quickly determine the original input based on the hashed output.
+
+
 import mysql from 'mysql2';
 import bcrypt from 'bcrypt';
 
@@ -12,7 +20,15 @@ const pool = mysql.createPool({
     database: 'users'
 }).promise()
 
-//function for adding new users to the database returns true if successful otherwise false
+//function for adding new users to the SQL database.
+//Takes 2 parameters userName and password
+//lowercases the username
+    //if username exist in database
+        //error is thrown and value of null is returned
+    //else 
+        //Password is hashed and salted using bcrypt
+        //SQL query is ran to create username with given userName and password 
+// returns username if successful otherwise Null
 export async function createUser(userName, password){
     let unique = true;
     let userNameLCase = userName.toLowerCase()
@@ -38,6 +54,7 @@ export async function createUser(userName, password){
    
 }
 
+//function to get the id of an user based on the username given
 export async function getUserIDByUserName(username){
     const [usernameInfo] = await pool.query(`
     select ID  
@@ -47,6 +64,7 @@ export async function getUserIDByUserName(username){
     return usernameInfo[0].ID;
 }
 
+
 export async function createUserInfo( ID,user_name, user_height, user_weight, user_target_calories){
     await pool.query(`
     INSERT INTO UserInfo (User_ID,User_name, User_height, User_weight, User_target_calories, User_preferences ) VALUES ( ?, ?, ?, ?, ?, NULL)
@@ -54,7 +72,7 @@ export async function createUserInfo( ID,user_name, user_height, user_weight, us
         `, [ID, user_name, user_height, user_weight, user_target_calories]);
 }
 
-//gets all userID and  passwords (Hashed+salted) from the data base and returns results as an object:
+//gets all userID and  passwords (Hashed+salted) from the sql data base and returns results as an object:
 // ID: id int,
 // Username: 'username' string,
 // Password: 'password' string(hashed),
@@ -64,7 +82,7 @@ export async function getUsers(){
     return result;
 }
 
-//gets Userinfo based on id from the data base and returns results as an object:
+//gets Userinfo from the SQL database based on id from the data base and returns results as an object:
 // {
 //User_ID: id int,
 // User_name: 'name' string,
@@ -106,7 +124,7 @@ export async function getfoodsByID(id){
     return result;
 }
 
-//creates an entry for the given food with the given parameters in the following format:
+//creates an entry in the SQL database for the given food with the given parameters in the following format:
 //Finds foods for the user based on their id and returns an object
 //{
 //User_ID: User_ID int,
